02181ca8899974d43ea4de97ee467fd1fa37c876
LCTyrell <loic.c.tyrell@gmail.com>
2020-07-14 11:27:27 +0200

    refactor : correction error in add duration (bis)

--- main.py
+++ main.py
@@ -22,23 +22,23 @@ def build_argparser():
     parser.add_argument("-mfd", "--model_fd", required=True, type=str,
                         help="Path to the face detection model file without extension (e.g. path/model_name).")
     parser.add_argument("-mhp", "--model_hp", required=True, type=str,
-                        help="Path to the head pose model file without extension (e.g. path/model_name).")    
+                        help="Path to the head pose model file without extension (e.g. path/model_name).")
     parser.add_argument("-mld", "--model_ld", required=True, type=str,
-                        help="Path to the landmark detection model file without extension (e.g. path/model_name).")     
+                        help="Path to the landmark detection model file without extension (e.g. path/model_name).")
     parser.add_argument("-mge", "--model_ge", required=True, type=str,
-                        help="Path to the gaze estimation model file without extension (e.g. path/model_name).")    
+                        help="Path to the gaze estimation model file without extension (e.g. path/model_name).")
     parser.add_argument("-i", "--input", required=True, type=str,
                         help="Use 'CAM' for camera or path to video file")
     parser.add_argument("-l", "--cpu_extension", required=False, type=str,
                         default=None,
-                        help="Path to cpu extension if needed.")  
+                        help="Path to cpu extension if needed.")
     parser.add_argument("-df", "--draw_flags", required=False, nargs='+',
                         default=[],
                         help="Flags to draw model(s) output on the video (e.g. fd hp ld ge)"
-                             "fd to draw face detection output" 
+                             "fd to draw face detection output"
                              "hp to draw head pose output"
                              "ld to draw landmark detection output"
-                             "ge to draw gaze estimation output" )  
+                             "ge to draw gaze estimation output" )
     parser.add_argument("-d", "--device", type=str, default="CPU",
                         help="Specify the target device: "
                              "CPU, GPU, FPGA, MYRIAD or MULTI (e.g. ""MULTI:CPU(2),GPU(2)"")")
@@ -46,44 +46,44 @@ def build_argparser():
                         help="Minimum inference probability threshold (0.5 by default)")
     return parser
 
-	
+
 def main():
     """
     Load the network and parse the SSD output.
     """
 
     args = build_argparser().parse_args()
-	
+
     model_fd=args.model_fd
     model_hp=args.model_hp
     model_ld=args.model_ld
     model_ge=args.model_ge
-    
+
     device=args.device
     draw_flags=args.draw_flags
     if args.input=='CAM':
         video_file=0
     else: video_file=args.input
-    
+
     threshold=args.threshold
-    
+
     mc=MouseController(precision='low', speed='fast')
 
     start_model_load_time=time.time()
-    
+
     log.info("Creating fd Inference Engine...")
-    ie = IECore()   
-    
+    ie = IECore()
+
     fd= Face_detection(model_fd, device, threshold)
     hp= Head_pose(model_hp, device, threshold)
     ld= Landmark_detection(model_ld, device, threshold)
     ge=Gaze_estimation(model_ge, device, threshold)
-    
+
     fd.load_model(ie)
     hp.load_model(ie)
     ld.load_model(ie)
     ge.load_model(ie)
-    
+
     total_model_load_time = time.time() - start_model_load_time
 
     try:
@@ -92,7 +92,7 @@ def main():
         print("Cannot locate video file: "+ video_file)
     except Exception as e:
         print("Something else went wrong with the video file: ", e)
-    
+
     initial_w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     initial_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = int(cap.get(cv2.CAP_PROP_FPS))
@@ -106,7 +106,7 @@ def main():
         fd.set_initial(initial_w, initial_h)
         hp.set_initial(initial_w, initial_h)
         ge.set_initial(initial_w, initial_h)
-        
+
         while cap.isOpened():
             ret, frame=cap.read()
             if not ret:
@@ -114,17 +114,17 @@ def main():
             counter+=1
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-            
+
             coords, image, head_image= fd.predict(frame, draw_flags)
- 
-            land_image, left_eye, right_eye, nose= ld.predict(head_image, draw_flags)            
+
+            land_image, left_eye, right_eye, nose= ld.predict(head_image, draw_flags)
             head_pose, pose_image= hp.predict(head_image, nose, draw_flags)
             eye_pose= ge.predict(left_eye, right_eye, head_pose, draw_flags)
-            
+
             if counter % 4 ==0:
                 mc.move(-eye_pose[0]/10, eye_pose[1]/10)
-            
-            cv2.imshow("Camera_view",cv2.resize(image,(900,450)))   
+
+            cv2.imshow("Camera_view",cv2.resize(image,(900,450)))
             #out_video.write(image)
 
         total_time=time.time()-start_inference_time
