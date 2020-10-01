6aa5ff2f036701ae1b087fc180a8164c184605e8
Łukasz Piłatowski <lukpila29@gmail.com>
2020-10-01 11:43:28 +0200

    eigth refactor branch

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import time
 from pathlib import Path
 import cv2
+import numpy as np
 from openvino.inference_engine import IENetwork, IECore
 from utils import draw_3d_axis
+
+
+def prepare_input(nnet, in_dict):
+    result = {}
+    for key in in_dict:
+        shape = nnet.inputs[key].shape
+        in_frame = np.array(in_dict[key])
+        if len(shape) == 4:
+            in_frame = cv2.resize(in_frame, tuple(shape[-2:]))
+            in_frame = in_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
+        result[key] = in_frame.reshape(shape)
+    return result
 
 
 class FaceDetection:
     def __init__(self, ie):
         self.model_weights = Path("models/face-detection-retail-0004/face-detection-retail-0004.bin").resolve().absolute()
         self.model_structure = self.model_weights.with_suffix('.xml')
-
-        self.model = IENetwork(str(self.model_structure), str(self.model_weights))
-
-        self.input_shape = self.model.inputs["data"].shape
-        self.output_shape = self.model.outputs["detection_out"].shape
         self.net = ie.read_network(model=str(self.model_structure), weights=str(self.model_weights))
         self.exec_net = ie.load_network(network=self.net, num_requests=0, device_name="MYRIAD")
 
     def predict(self, image):
-        height, width = image.shape[:2]
-        input_dict = {}
-        n, c, h, w = self.input_shape
-        in_frame = cv2.resize(image, (w, h))
-        in_frame = in_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
-        in_frame = in_frame.reshape((n, c, h, w))
-        input_dict["data"] = in_frame
-        self.exec_net.start_async(request_id=0, inputs=input_dict)
+        self.exec_net.start_async(request_id=0, inputs=prepare_input(self.exec_net, {"data": image}))
         while True:
             status = self.exec_net.requests[0].wait(-1)
             if status == 0:
@@ -33,6 +34,7 @@ class FaceDetection:
             else:
                 time.sleep(0.1)
         coords = []
+        height, width = image.shape[:2]
         for obj in self.exec_net.requests[0].outputs["detection_out"][0][0]:
             if obj[2] > 0.6:
                 xmin = int(obj[3] * width)
@@ -53,17 +55,9 @@ class LandmarkDetection:
         self.model_structure = self.model_weights.with_suffix('.xml')
         self.net2 = ie.read_network(model=str(self.model_structure), weights=str(self.model_weights))
         self.exec_net = ie.load_network(network=self.net2, num_requests=0, device_name="MYRIAD")
-        self.input_shape = self.exec_net.inputs["0"].shape
-        self.output_shape = self.exec_net.outputs["95"].shape
 
     def predict(self, image):
-        input_dict = {}
-        n, c, h, w = self.input_shape
-        in_frame = cv2.resize(image, (w, h))
-        in_frame = in_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
-        in_frame = in_frame.reshape((n, c, h, w))
-        input_dict["0"] = in_frame
-        self.exec_net.start_async(request_id=0, inputs=input_dict)
+        self.exec_net.start_async(request_id=0, inputs=prepare_input(self.exec_net, {"0": image}))
         while True:
             status = self.exec_net.requests[0].wait(-1)
             if status == 0:
@@ -101,21 +95,11 @@ class HeadPose:
     def __init__(self, ie):
         self.model_weights = Path("models/head-pose-estimation-adas-0001/head-pose-estimation-adas-0001.bin").resolve().absolute()
         self.model_structure = self.model_weights.with_suffix('.xml')
-        self.model = IENetwork(str(self.model_structure), str(self.model_weights))
-        self.input_shape = self.model.inputs["data"].shape
-        self.output_shape = self.model.outputs["angle_p_fc"].shape
         self.net3 = ie.read_network(model=str(self.model_structure), weights=str(self.model_weights))
         self.exec_net = ie.load_network(network=self.net3, num_requests=0, device_name="MYRIAD")
 
     def predict(self, image, origin):
-        height, width = image.shape[:2]
-        input_dict = {}
-        n, c, h, w = self.input_shape
-        in_frame = cv2.resize(image, (w, h))
-        in_frame = in_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
-        in_frame = in_frame.reshape((n, c, h, w))
-        input_dict["data"] = in_frame
-        self.exec_net.start_async(request_id=0, inputs=input_dict)
+        self.exec_net.start_async(request_id=0, inputs=prepare_input(self.exec_net, {"data": image}))
         while True:
             status = self.exec_net.requests[0].wait(-1)
             if status == 0:
@@ -129,6 +113,7 @@ class HeadPose:
             self.exec_net.requests[0].outputs['angle_y_fc']
         )
 
+        height, width = image.shape[:2]
         draw_3d_axis(image, head_pose[2], head_pose[1], head_pose[0], int(origin[0] * width), int(origin[1] * height))
         return head_pose, image
 
@@ -139,15 +124,13 @@ class GazeEstimation:
         self.model_structure = self.model_weights.with_suffix('.xml')
         self.net4 = ie.read_network(model=str(self.model_structure), weights=str(self.model_weights))
         self.exec_net = ie.load_network(network=self.net4, num_requests=0, device_name="MYRIAD")
-        self.input_shape = self.exec_net.inputs['left_eye_image'].shape
 
-    def predict(self, r_eye, l_eye, pose):
-        input_dict = {}
-        input_dict['left_eye_image'] = self.preprocess_input(r_eye)
-        input_dict['right_eye_image'] = self.preprocess_input(l_eye)
-        input_dict['head_pose_angles'] = [pose]
-
-        self.exec_net.start_async(request_id=0, inputs=input_dict)
+    def predict(self, l_eye, r_eye, pose):
+        self.exec_net.start_async(request_id=0, inputs=prepare_input(self.exec_net, {
+            "left_eye_image": l_eye,
+            "right_eye_image": r_eye,
+            "head_pose_angles": pose
+        }))
         while True:
             status = self.exec_net.requests[0].wait(-1)
             if status == 0:
@@ -168,16 +151,6 @@ class GazeEstimation:
         cv2.arrowedLine(l_eye, (origin_x_le, origin_y_le), (origin_x_le + x, origin_y_le - y), (255, 0, 255), 3)
         cv2.arrowedLine(r_eye, (origin_x_re, origin_y_re), (origin_x_re + x, origin_y_re - y), (255, 0, 255), 3)
         return coords
-
-    def preprocess_input(self, image):
-        try:
-            n, c, h, w = self.input_shape
-            in_frame = cv2.resize(image, (w, h))
-            in_frame = in_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
-            in_frame = in_frame.reshape((n, c, h, w))
-            return in_frame
-        except Exception as e:
-            print(str(e))
 
 
 if __name__ == '__main__':
@@ -206,8 +179,9 @@ if __name__ == '__main__':
 
         land_image, left_eye, right_eye, nose = ld.predict(head_image)
         head_pose, pose_image = hp.predict(head_image, nose)
-        eye_pose = ge.predict(left_eye, right_eye, head_pose)
-        print(eye_pose)
+        if left_eye.size > 0 and right_eye.size > 0:
+            eye_pose = ge.predict(left_eye, right_eye, head_pose)
+            print(eye_pose)
         cv2.imshow("Camera_view", cv2.resize(image, (900, 450)))
 
     cap.release()
