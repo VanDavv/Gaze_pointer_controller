0737014cc5c73e03b30b708584783db1a23ba805
Łukasz Piłatowski <lukpila29@gmail.com>
2020-10-01 12:21:39 +0200

    nineth refactor branch

--- main.py
+++ main.py
@@ -18,6 +18,17 @@ def prepare_input(nnet, in_dict):
     return result
 
 
+def run_network(nnet, in_dict):
+    nnet.start_async(request_id=0, inputs=prepare_input(nnet, in_dict))
+    while nnet.requests[0].wait(-1) != 0:
+        time.sleep(0.1)
+    result = {
+        key: nnet.requests[0].outputs[key][0]
+        for key in nnet.requests[0].outputs
+    }
+    return result
+
+
 class FaceDetection:
     def __init__(self, ie):
         self.model_weights = Path("models/face-detection-retail-0004/face-detection-retail-0004.bin").resolve().absolute()
@@ -26,27 +37,17 @@ class FaceDetection:
         self.exec_net = ie.load_network(network=self.net, num_requests=0, device_name="MYRIAD")
 
     def predict(self, image):
-        self.exec_net.start_async(request_id=0, inputs=prepare_input(self.exec_net, {"data": image}))
-        while True:
-            status = self.exec_net.requests[0].wait(-1)
-            if status == 0:
-                break
-            else:
-                time.sleep(0.1)
-        coords = []
+        out = run_network(self.exec_net, {"data": image})
         height, width = image.shape[:2]
-        for obj in self.exec_net.requests[0].outputs["detection_out"][0][0]:
-            if obj[2] > 0.6:
-                xmin = int(obj[3] * width)
-                ymin = int(obj[4] * height)
-                xmax = int(obj[5] * width)
-                ymax = int(obj[6] * height)
-                coords.append((xmin, ymin, xmax, ymax))
-
+        coords = [
+            (int(obj[3] * width), int(obj[4] * height), int(obj[5] * width), int(obj[6] * height))
+            for obj in out["detection_out"][0]
+            if obj[2] > 0.6
+        ]
         head_image = image[coords[0][1]:coords[0][3], coords[0][0]:coords[0][2]]
         for obj in coords:
             cv2.rectangle(image, (obj[0], obj[1]), (obj[2], obj[3]), (10, 245, 10), 2)
-        return coords, image, head_image
+        return head_image
 
 
 class LandmarkDetection:
@@ -57,25 +58,8 @@ class LandmarkDetection:
         self.exec_net = ie.load_network(network=self.net2, num_requests=0, device_name="MYRIAD")
 
     def predict(self, image):
-        self.exec_net.start_async(request_id=0, inputs=prepare_input(self.exec_net, {"0": image}))
-        while True:
-            status = self.exec_net.requests[0].wait(-1)
-            if status == 0:
-                break
-            else:
-                time.sleep(0.1)
-        right_eye = (
-             self.exec_net.requests[0].outputs["95"][0][0],
-             self.exec_net.requests[0].outputs["95"][0][1],
-        )
-        left_eye = (
-             self.exec_net.requests[0].outputs["95"][0][2],
-             self.exec_net.requests[0].outputs["95"][0][3],
-        )
-        nose = (
-             self.exec_net.requests[0].outputs["95"][0][4],
-             self.exec_net.requests[0].outputs["95"][0][5],
-        )
+        out = run_network(self.exec_net, {"0": image})
+        right_eye, left_eye, nose = out["95"][:2], out["95"][2:4], out["95"][4:]
         w = image.shape[1]
         h = image.shape[0]
 
@@ -83,12 +67,10 @@ class LandmarkDetection:
         left_eye_image = image[int(left_eye[1]*h) - 30:int(left_eye[1]*h) + 30, int(left_eye[0]*w) - 30:int(left_eye[0]*w) + 30]
 
         cv2.circle(image, (int(nose[0] * w), int(nose[1] * h)), 2, (0, 255, 0), thickness=5, lineType=8, shift=0)
-        cv2.rectangle(image, (right_eye[0] * w - 30, right_eye[1] * h - 30),
-                      (right_eye[0] * w + 30, right_eye[1] * h + 30), (245, 245, 245), 2)
-        cv2.rectangle(image, (left_eye[0] * w - 30, left_eye[1] * h - 30),
-                      (left_eye[0] * w + 30, left_eye[1] * h + 30), (245, 245, 245), 2)
+        cv2.rectangle(image, (right_eye[0] * w - 30, right_eye[1] * h - 30), (right_eye[0] * w + 30, right_eye[1] * h + 30), (245, 245, 245), 2)
+        cv2.rectangle(image, (left_eye[0] * w - 30, left_eye[1] * h - 30), (left_eye[0] * w + 30, left_eye[1] * h + 30), (245, 245, 245), 2)
 
-        return image, left_eye_image, right_eye_image, nose
+        return left_eye_image, right_eye_image, nose
 
 
 class HeadPose:
@@ -99,23 +81,11 @@ class HeadPose:
         self.exec_net = ie.load_network(network=self.net3, num_requests=0, device_name="MYRIAD")
 
     def predict(self, image, origin):
-        self.exec_net.start_async(request_id=0, inputs=prepare_input(self.exec_net, {"data": image}))
-        while True:
-            status = self.exec_net.requests[0].wait(-1)
-            if status == 0:
-                break
-            else:
-                time.sleep(0.1)
-
-        head_pose = (
-            self.exec_net.requests[0].outputs['angle_r_fc'],
-            self.exec_net.requests[0].outputs['angle_p_fc'],
-            self.exec_net.requests[0].outputs['angle_y_fc']
-        )
-
+        out = run_network(self.exec_net, {"data": image})
+        head_pose = [value[0] for value in out.values()]
         height, width = image.shape[:2]
         draw_3d_axis(image, head_pose[2], head_pose[1], head_pose[0], int(origin[0] * width), int(origin[1] * height))
-        return head_pose, image
+        return head_pose
 
 
 class GazeEstimation:
@@ -126,31 +96,21 @@ class GazeEstimation:
         self.exec_net = ie.load_network(network=self.net4, num_requests=0, device_name="MYRIAD")
 
     def predict(self, l_eye, r_eye, pose):
-        self.exec_net.start_async(request_id=0, inputs=prepare_input(self.exec_net, {
+        out = run_network(self.exec_net, {
             "left_eye_image": l_eye,
             "right_eye_image": r_eye,
             "head_pose_angles": pose
-        }))
-        while True:
-            status = self.exec_net.requests[0].wait(-1)
-            if status == 0:
-                break
-            else:
-                time.sleep(1)
-        coords = (
-            self.exec_net.requests[0].outputs['gaze_vector'][0][0],
-            self.exec_net.requests[0].outputs['gaze_vector'][0][1],
-            self.exec_net.requests[0].outputs['gaze_vector'][0][2],
-        )
+        })
+
         origin_x_re = r_eye.shape[1] // 2
         origin_y_re = r_eye.shape[0] // 2
         origin_x_le = l_eye.shape[1] // 2
         origin_y_le = l_eye.shape[0] // 2
 
-        x, y = int(coords[0] * 100), int(coords[1] * 100)
+        x, y = (out["gaze_vector"] * 100).astype(int)[:2]
         cv2.arrowedLine(l_eye, (origin_x_le, origin_y_le), (origin_x_le + x, origin_y_le - y), (255, 0, 255), 3)
         cv2.arrowedLine(r_eye, (origin_x_re, origin_y_re), (origin_x_re + x, origin_y_re - y), (255, 0, 255), 3)
-        return coords
+        return out["gaze_vector"]
 
 
 if __name__ == '__main__':
@@ -175,14 +135,13 @@ if __name__ == '__main__':
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-        coords, image, head_image = fd.predict(frame)
-
-        land_image, left_eye, right_eye, nose = ld.predict(head_image)
-        head_pose, pose_image = hp.predict(head_image, nose)
+        head_image = fd.predict(frame)
+        left_eye, right_eye, nose = ld.predict(head_image)
+        head_pose = hp.predict(head_image, nose)
         if left_eye.size > 0 and right_eye.size > 0:
             eye_pose = ge.predict(left_eye, right_eye, head_pose)
             print(eye_pose)
-        cv2.imshow("Camera_view", cv2.resize(image, (900, 450)))
+        cv2.imshow("Camera_view", cv2.resize(frame, (900, 450)))
 
     cap.release()
     cv2.destroyAllWindows()
