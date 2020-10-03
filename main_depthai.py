04a0261486798998f469971f024c8b51e20ee172
Łukasz Piłatowski <lukpila29@gmail.com>
2020-10-03 11:20:19 +0200

    add nnet read

--- main_depthai.py
+++ main_depthai.py
@@ -6,6 +6,7 @@ import numpy as np
 
 print('Using depthai module from: ', depthai.__file__, ' version: ', depthai.__version__)
 
+
 print("Creating pipeline...")
 p = depthai.Pipeline()
 
@@ -23,13 +24,12 @@ cam.preview.link(cam_xout.input)
 # NeuralNetwork
 print("Creating Neural Network...")
 nn = p.createNeuralNetwork()
-nn.setBlobPath(str(Path("models/face-detection-retail-0004/face-detection-retail-0004.bin").resolve().absolute()))
+nn.setBlobPath(str(Path("models/face-detection-retail-0004/face-detection-retail-0004.blob").resolve().absolute()))
 nn_xout = p.createXLinkOut()
 nn_xout.setStreamName("face_nn")
 cam.preview.link(nn.input)
 nn.out.link(nn_xout.input)
 print("Pipeline created.")
-
 
 
 # Get first device and connect
@@ -41,10 +41,28 @@ if found:
     print("Starting pipeline...")
     device.startPipeline(p)
     preview = device.getOutputQueue("preview")
+    face_nn = device.getOutputQueue("face_nn")
     print("Pipeline started.")
     while True:
-        data = preview.get()
-        cv2.imshow("test", np.array(data.data).reshape((300, 300, 3)).astype(np.uint8))
+        face_raw_output = face_nn.get().data
+        data = [
+            {
+                "conf": face_raw_output[i * 7],
+                "label": face_raw_output[i * 7 + 1],
+                "score": face_raw_output[i * 7 + 2],
+                "x_min": face_raw_output[i * 7 + 3],
+                "y_min": face_raw_output[i * 7 + 4],
+                "x_max": face_raw_output[i * 7 + 5],
+                "y_max": face_raw_output[i * 7 + 6],
+            }
+            for i in range(len(face_raw_output) // 7)
+            if face_raw_output[i * 7] != -1
+        ]
+        print(len(data), data)
+
+        preview_frame = preview.get()
+        cv2.imshow("test", np.array(preview_frame.data).reshape((300, 300, 3)).astype(np.uint8))
+
 
         if cv2.waitKey(1) == ord("q"):
             break
